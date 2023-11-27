b77e12b687e992df412b52cc1a1f833aea26c2ec
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-11-27 18:52:40 +0100

    Add project files.

---
+++ Inlämningsuppgift 5.6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace uppgift_5._6
+{
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Skriv in tal separerade med \"*\"");
+                string[] input = Console.ReadLine().Split("*");
+                int[] tal = new int[input.Length];
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    tal[i] = int.Parse(input[i]);
+                }
+
+                long produkt = 1;
+
+                for (int i = 0; i < tal.Length; i++)
+                {
+                    produkt *= tal[i];
+                }
+                Console.WriteLine(produkt);
+            }
+
+            catch
+            {
+                Console.WriteLine("Fel input, vänligen pröva igen");
+                Main();
+            }
+        }
+    }
+}
